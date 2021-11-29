379b37d9fbade3eae65ae45f2a09ace101ad8b61
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-11-29 08:36:14 -0800

    commit tree e7014684813a58ff296808cfc7b599b2c14c65d4

--- phase-3/oo-ruby-review/lib/computer.rb
+++ phase-3/oo-ruby-review/lib/computer.rb
@@ -1,6 +1,52 @@
 require 'pry'
 
-# Your code goes here
+class Computer
+
+
+
+    def initialize(brand, model)
+        @memory_GB = 8
+        @storage_free = 1000
+        @brand = brand
+        @model = model
+    end
+
+    def brand 
+      self.brand  
+    end
+
+    def model
+
+    end
+
+
+    def memory_GB
+
+    end
+
+
+    def memory_GB
+
+    end
+
+
+    def memory_GB=()
+
+    end
+
+
+    def storage_free
+
+    end
+
+
+    def storage_free=()
+
+    end
+
+end
+
+
 
 binding.pry
 0
